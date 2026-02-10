d52741a83816c42d4f2cca5a273d31496b1cdd8a
Tony Ramos <158001213+TonyRamos1982@users.noreply.github.com>
2026-02-09 23:57:48 -0600

    Create WN11-AU-000050.ps1

---
+++ STIGS/WN11-AU-000050.ps1
@@ -0,0 +1,44 @@
+<#
+.SYNOPSIS
+    WN11-AU-000050 Remediation
+    Enable auditing for Detailed Tracking - Process Creation (Success)
+
+.NOTES
+    Author          : Tony Ramos
+    LinkedIn        : linkedin.com/in/tony-ramos1982/
+    GitHub          : github.com/tonyramos1982
+    Date Created    : 2026-02-09
+    Last Modified   : 2026-02-09
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000050
+
+.TESTED ON
+    Date(s) Tested  : 2026-02-09
+    Tested By       : Tony Ramos
+    Systems Tested  : windows11
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN11-CC-000050.ps1
+#>
+
+# Ensure script is running as Administrator
+If (-not ([Security.Principal.WindowsPrincipal] `
+    [Security.Principal.WindowsIdentity]::GetCurrent()
+    ).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Error "This script must be run as Administrator."
+    Exit 1
+}
+
+# Enable Process Creation auditing (Success)
+auditpol /set /subcategory:"Process Creation" /success:enable
+
+# Confirm configuration
+$auditStatus = auditpol /get /subcategory:"Process Creation"
+
+Write-Output "WN11-AU-000050 remediation applied."
+Write-Output $auditStatus
