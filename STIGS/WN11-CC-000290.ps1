fb501f330855660a25a71e0714b846a12a9f0ee7
Tony Ramos <158001213+TonyRamos1982@users.noreply.github.com>
2026-02-10 21:11:09 -0600

    Create WN11-CC-000290.ps1

---
+++ STIGS/WN11-CC-000290.ps1
@@ -0,0 +1,54 @@
+<#
+.SYNOPSIS
+    WN11-CC-000290 Remediation
+    Disable WinRM Service Digest authentication
+
+.NOTES
+    Author          : Tony Ramos
+    LinkedIn        : linkedin.com/in/tony-ramos1982/
+    GitHub          : github.com/tonyramos1982
+    Date Created    : 2026-02-10
+    Last Modified   : 2026-02-10
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000290
+
+.TESTED ON
+    Date(s) Tested  : 2026-02-10
+    Tested By       : Tony Ramos
+    Systems Tested  : windows11
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN11-CC-000290.ps1
+#>
+
+# Ensure script is running as Administrator
+If (-not ([Security.Principal.WindowsPrincipal] `
+    [Security.Principal.WindowsIdentity]::GetCurrent()
+    ).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Error "This script must be run as Administrator."
+    Exit 1
+}
+
+$RegPath   = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Service"
+$ValueName = "AllowDigest"
+$ValueData = 0
+
+# Create registry path if it does not exist
+if (-not (Test-Path $RegPath)) {
+    New-Item -Path $RegPath -Force | Out-Null
+}
+
+# Set required value
+New-ItemProperty `
+    -Path $RegPath `
+    -Name $ValueName `
+    -PropertyType DWORD `
+    -Value $ValueData `
+    -Force | Out-Null
+
+Write-Output "WN11-CC-000290 remediation applied successfully."
